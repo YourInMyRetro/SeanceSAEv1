072a85a355632ed9c3f4d5adcd35d4f3421ecdb8
pasquiev <pasquiev@D353-S14>
2024-12-09 16:40:37 +0100

    Ajoutez des fichiers projet.

---
+++ version1_SAE_1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace version1_SAE_1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
